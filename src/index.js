503a5f4623c4e604ae026298da06eaf24aa8ad7f
ablaoublas11 <50484255+ablaoublas11@users.noreply.github.com>
2026-09-02 19:54:56 +0000

    Some changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles/main.css";
-import { AppController } from "./logic/AppController";
+import { AppController } from "./logic/AppController.js";
 import { initEventListeners } from "./ui/eventListeners";
 //πρέπει να γίνει import το eventListener.js
 
